9f9bb6d38d4898c4c114dd14570773ed977af146
Egor Orlov <egororloff@gmail.com>
2023-12-19 22:32:47 +0300

    init

---
+++ src/constats.ts
@@ -0,0 +1,4 @@
+export const SITE_TITLE = 'TechPeople';
+export const SITE_DESCRIPTION = 'Articles, Stories & Tutorials for Tech People';
+export const HOMEPAGE_ARTICLE_LIMIT = 7;
+export const ARTICLES_PER_PAGE= 6;
